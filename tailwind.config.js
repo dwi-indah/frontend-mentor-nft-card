70d418a09943511140ddee0e8431b89c72c81acb
Dwi Indah <dwindach@gmail.com>
2022-01-19 16:13:00 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans' : ['outfit'],
+    },
+    colors: {
+      primary: {
+        50: '#ffffff',
+        100: '#8bacda',
+        200: '#00fff7',
+        300: '#93c5fd',
+        400: '#60a5fa',
+        500: '#3b82f6',
+        600: '#2563eb',
+        700: '#2f415b',
+        800: '#14253d',
+        900: '#0d192b',
+      },
+    },
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['18px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    }
+  },
+  plugins: [],
+}
